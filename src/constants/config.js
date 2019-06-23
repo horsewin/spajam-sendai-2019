f91d1c06dc50b31691770d6bd3cc0ccd17fff3b3
horsewin <mamamatz@gmail.com>
2019-06-23 19:14:14 +0900

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,9 +3,6 @@ export default {
   tabHeight: 85,
   flatlistHeight: 570,
   floatingButtonMargin: 10,
-  api: {
-    baseUrl: "https://us-central1-spajam2019-sendai.cloudfunctions.net"
-  },
   color: {
     primaryColor: "#F4A626",
     whiteColor: "#FFFFFF",
